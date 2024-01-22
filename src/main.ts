687880c58f29a5ff104ca7c82e6de1d9d60e9efa
Jørgen Vik <mail@jvik.no>
2024-01-22 23:23:21 +0100

    Cleaned up old names

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import playersRouter from './player/playersRouter.js';
-import bidRouter from './bid/bidsRouter.js';
+import playersRouter from './players/playersRouter.js';
+import bidRouter from './bids/bidsRouter.js';
 
 const app = express();
 const port = 3000;
