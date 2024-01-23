d75647389d76cdfaf33a160ac3d32e03a7b6c2dc
Jørgen Vik <mail@jvik.no>
2024-01-23 12:25:06 +0100

    Update src/bids/bid.ts
    Co-authored-by: AV-AER <153427222+AV-AER@users.noreply.github.com>

--- src/bids/bid.ts
+++ src/bids/bid.ts
@@ -1,12 +1,14 @@
 import Player from "@/players/player.js";
 
 class Bid {
-  suit: "spades" | "hearts" | "diamonds" | "clubs" | "no-trump" | "pass";
+  pass: boolean;
+  suit: "spades" | "hearts" | "diamonds" | "clubs" | "no-trump";
   // Using number because that will make it easier to compare
   level: number;
   player: Player;
 
-  constructor(suit, level) {
+  constructor(pass, suit, level) {
+    this.pass = pass;
     this.suit = suit;
     this.level = level;
   }
