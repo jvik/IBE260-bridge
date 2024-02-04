c51fa1ca282cdb867c7fe1165291594ad4d5816e
AV-AER <153427222+AV-AER@users.noreply.github.com>
2024-02-04 22:11:20 +0100

    Update src/table/rules.ts
    Co-authored-by: Jørgen Vik <mail@jvik.no>

--- src/table/rules.ts
+++ src/table/rules.ts
@@ -5,7 +5,7 @@ class Rules {
   ruleRank: number;
   rule: string;
 
-  constructor(ruleSuit:"spades" | "hearts" | "diamonds" | "clubs" | "no-trump", ruleRank: number, rule: string ) {
+  constructor(ruleSuit: CardSuit, ruleRank: number, rule: string ) {
     this.ruleSuit = ruleSuit;
     this.ruleRank = ruleRank;
     this.rule = rule;
