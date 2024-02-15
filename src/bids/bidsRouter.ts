8fba31d810270375d2f3a9c9cb6783e0b9888a05
Jørgen Vik <mail@jvik.no>
2024-02-15 22:16:33 +0100

    Test githook

--- src/bids/bidsRouter.ts
+++ src/bids/bidsRouter.ts
@@ -1,27 +1,27 @@
-import express from 'express';
-import Bid from '@/bids/bid.js';
-import BidLog from '@/bids/bidLog.js';
-import Table from '@/table/table.js';
+import Bid from "@/bids/bid.js";
+import BidLog from "@/bids/bidLog.js";
+import Table from "@/table/table.js";
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
+router.get("/", (_req, res) => {
   const bidLog = BidLog.getInstance();
   res.json(bidLog.getBidLog());
 });
 
-router.post('/bid', (req, res) => {
+router.post("/bid", (req, res) => {
   const { bid, pass, playerName } = req.body;
   const bidLog = BidLog.getInstance();
   let roundOver = bidLog.isBiddingOver();
 
   const ourTable = Table.getInstance();
   if (ourTable.getPlayers().length < 4) {
-    throw new Error('The table is not full');
+    throw new Error("The table is not full");
   }
-  
+
   let ourBid = undefined;
-  if ((bid?.suit && bid?.rank) || bid?.pass) {
+  if ((bid?.suit && bid?.rank) || bid.pass) {
     ourBid = new Bid(playerName, bid.suit, bid.rank, pass);
     if (!roundOver) {
       bidLog.addBid(ourBid);
