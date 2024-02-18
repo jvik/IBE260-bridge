46df5246363bea0c3638f56d268f22d63b76fa3f
Jørgen Vik <mail@jvik.no>
2024-02-19 00:07:10 +0100

    Added helpful comment

--- server/src/cards/cardsRouter.ts
+++ server/src/cards/cardsRouter.ts
@@ -3,6 +3,8 @@ import Deck from "./deck.js";
 
 const router = express.Router();
 
+// Only for debugging purposes to check if cards are dealt to players
+// If cards are dealt correctly, this should not be needed
 router.get("/", (_req, res) => {
   res.send(Deck.getInstance());
 });
